cb746027dd33fe6aec86167caf9bfc8c8294ac66
Lussati Santos <lussatisantos10@gmail.com>
2023-08-09 15:19:19 +0100

    testando o comportamento

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from pessoa import Pessoa
 p1 = Pessoa('Lussati', 20)
 p2 = Pessoa('Santos', 19)
 
+
 p2.comer('banana')
+p2.falar('POO')
 p2.parar_comer()
-p2.parar_comer()
-p2.comer('banana')
+p2.falar('POO')
--- pessoa.py
+++ pessoa.py
@@ -5,6 +5,18 @@ class Pessoa:
         self.comendo = comendo
         self.falando = falando
 
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome} nao pode falar comendo')
+            return
+        
+        if self.falando:
+            print(f'{self.nome} ja esta falando')
+            return
+        
+        print(f'{self.nome} esta falando')
+        self.falando = True
+
     def comer(self, alimento):
         if self.comendo:
             print(f'{self.nome} ja esta comendo')
@@ -16,5 +28,5 @@ class Pessoa:
         if not self.comendo:
             print(f'{self.nome} nao esta comendo')
             return
-        print(f'{self.nome} nao parou de comer')
+        print(f'{self.nome} parou de comer')
         self.comendo = False
