7ace7a79af26e4edd032270018b396933cf9eb7f
Lussati Santos <lussatisantos10@gmail.com>
2023-08-09 15:26:26 +0100

    comportamentos

--- pessoa.py
+++ pessoa.py
@@ -14,13 +14,25 @@ class Pessoa:
             print(f'{self.nome} ja esta falando')
             return
         
-        print(f'{self.nome} esta falando')
+        print(f'{self.nome} esta falando sobre {assunto}')
         self.falando = True
+
+    def parar_falar(self):
+        if not self.falando:
+            print(f'{self.nome} nao esta falando')
+            return
+        print(f'{self.nome} parou de falar')
+        self.falando = False
 
     def comer(self, alimento):
         if self.comendo:
             print(f'{self.nome} ja esta comendo')
             return
+        
+        if self.falando:
+            print(f'{self.nome} nao pode comer falando')
+            return
+        
         print(f'{self.nome} esta comendo {alimento}')
         self.comendo = True
     
