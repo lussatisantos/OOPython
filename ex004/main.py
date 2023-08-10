7a08efe3fdeb440ee5bbcb9dd13c9a136a4f13da
Lussati Santos <lussatisantos10@gmail.com>
2023-08-10 20:25:24 +0100

    setters e getters

--- ex004/main.py
+++ ex004/main.py
@@ -6,6 +6,18 @@ class Produto:
     def desconto(self, percentual):
         self.preco = self.preco - (self.preco * percentual / 100)
 
+    # Getter
+    @property
+    def preco(self):
+        return self._preco
+
+    #Setter
+    @preco.setter
+    def preco(self, valor):
+        if isinstance(valor, str):
+            valor = float(valor.replace('AOA', ''))
+        self._preco = valor
+
 p1 = Produto('Camisa', 2500)
 p1.desconto(7)
 print(p1.nome, p1.preco)
