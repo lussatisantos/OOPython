a0c02eb618e64854d67df388f3e879eeb3c44c76
Lussati Santos <lussatisantos10@gmail.com>
2023-08-10 20:03:27 +0100

    Update main.py

--- ex002/main.py
+++ ex002/main.py
@@ -8,4 +8,12 @@ class Pessoa:
     def get_ano_nascimento(self):
         print(self.ano_actual - self.idade)
     
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimento):
+        idade = cls.ano_actual - ano_nascimento
+        return cls(nome, idade)
     
+
+p1 = Pessoa.por_ano_nascimento('Paulo', 2001)
+print(p1.nome, p1.idade)
+p1.get_ano_nascimento()
