9ee4199c72fa63e5590c092f6de7991905331057
Lussati Santos <lussatisantos10@gmail.com>
2023-08-10 20:19:29 +0100

    aprendendo setters e getters

--- ex004/main.py
+++ ex004/main.py
@@ -8,8 +8,8 @@ class Produto:
 
 p1 = Produto('Camisa', 2500)
 p1.desconto(7)
-print(p1.preco)
+print(p1.nome, p1.preco)
 
-p2 = Produto('Caneca', 4500)
+p2 = Produto('Caneca', 'AOA 4500')
 p2.desconto(20)
-print(p2.preco)
+print(p2.nome, p2.preco)
